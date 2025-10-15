89881f6979f3f8e85ad8333503afcca0b1cae8f1
suidonx <neesama178@gmail.com>
2025-10-16 05:29:59 +0900

    urlsにallauthのaccounts/を追加

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
-from django.views.generic.base import TemplateView
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', TemplateView.as_view(template_name='hello.html')),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
 ]
