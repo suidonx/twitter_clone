4123428ae9ca403ae0ad181696bce37c45094298
suidonx <neesama178@gmail.com>
2025-10-21 22:47:09 +0900

    heroku環境でのメディアファイル設定の追加

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,3 +3,17 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = [".herokuapp.com"]
+
+INSTALLED_APPS += [
+    "cloudinary",
+    "cloudinary_storage",
+]
+
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
