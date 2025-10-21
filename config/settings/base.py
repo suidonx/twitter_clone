fb2f5955a3cfb72d62b9c7023b3f28b43b4db9ef
suidonx <neesama178@gmail.com>
2025-10-21 22:55:16 +0900

    INSTALLED_APPSに重複して記述していたのを削除

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,7 +37,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "whitenoise.runserver _nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
