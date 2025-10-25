14d2030f1682a79860eb088e36595d04118d06c9
suidonx <neesama178@gmail.com>
2025-10-25 15:41:57 +0900

    トップページをpostsアプリで制御

--- config/urls.py
+++ config/urls.py
@@ -19,12 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path("", TemplateView.as_view(template_name="top.html")),
+    path("", include("posts.urls")),
 ]
 
 if settings.DEBUG:
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Tweet
+
 
 # Create your views here.
+class IndexView(ListView):
+    model = Tweet
+    template_name = "posts/top.html"
