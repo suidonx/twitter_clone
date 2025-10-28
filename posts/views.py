b23b66568287924a37a573cc00a6f416ad876972
suidonx <neesama178@gmail.com>
2025-10-28 16:07:59 +0900

    タブ切り替えをaタグとクエリパラメータによる実装に修正

--- posts/views.py
+++ posts/views.py
@@ -2,9 +2,30 @@ from django.shortcuts import render
 from django.views.generic import ListView
 
 from .models import Tweet
+from users.models import Follow
 
 
 # Create your views here.
 class IndexView(ListView):
     model = Tweet
     template_name = "posts/top.html"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        parm = self.request.GET.get("tab", "recommend")
+
+        if parm == "recommend":
+            queryset = Tweet.objects.prefetch_related("user").order_by("-created_at")
+
+        elif parm == "follow":
+            users = Follow.objects.filter(follower=self.request.user).values_list(
+                "followed"
+            )
+
+            queryset = (
+                Tweet.objects.filter(user__in=users)
+                .prefetch_related("user")
+                .order_by("-created_at")
+            )
+
+        return queryset
