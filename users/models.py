13893fc0b8a6c8cec4800f49628ae99315e8a562
suidonx <neesama178@gmail.com>
2025-11-05 14:40:48 +0900

    コメントの最大文字数を設定

--- users/models.py
+++ users/models.py
@@ -79,6 +79,6 @@ class Comment(models.Model):
         Tweet,
         on_delete=models.CASCADE,
     )
-    content = models.TextField()
+    content = models.TextField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
