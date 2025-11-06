296ac38691ffb9763c343661253b9d04e2893435
suidonx <neesama178@gmail.com>
2025-11-07 01:47:31 +0900

    django-debug-toolbarの導入

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,1 +1,15 @@
 from .base import *
+
+INSTALLED_APPS += [
+    "debug_toolbar",
+]
+
+MIDDLEWARE += [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = ["127.0.0.1"]
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": lambda request: True,
+}
