df47faff86e9720a1d222ea37790e5880f8e02c3
suidonx <neesama178@gmail.com>
2025-11-17 03:49:21 +0900

    管理サイトで通知モデルを表示

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-from .models import Follow, Like, Retweet, Comment, Message
+from .models import (
+    Follow,
+    Like,
+    Retweet,
+    Comment,
+    Message,
+    LikeNotify,
+    RetweetNotify,
+    CommentNotify,
+)
 
 # Register your models here.
 admin.site.register(Follow)
@@ -8,3 +17,6 @@ admin.site.register(Like)
 admin.site.register(Retweet)
 admin.site.register(Comment)
 admin.site.register(Message)
+admin.site.register(LikeNotify)
+admin.site.register(RetweetNotify)
+admin.site.register(CommentNotify)
